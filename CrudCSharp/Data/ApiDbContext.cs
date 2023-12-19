7ccb1422c7bc957d9aaf9c2d2c1b68b80a95263a
dantzs <marcelohrs2005@gmail.com>
2023-12-19 14:24:15 -0300

    chore: Conectando com o banco de dados

--- CrudCSharp/Data/ApiDbContext.cs
+++ CrudCSharp/Data/ApiDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CrudCSharp.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 
 namespace CrudCSharp.Data
 {
@@ -7,6 +9,8 @@ namespace CrudCSharp.Data
         public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) 
         {
         }
+
+        public DbSet<Produto> Produtos { get; set; } //criando tabela com as propriedades da classe Produto
 
 
     }
