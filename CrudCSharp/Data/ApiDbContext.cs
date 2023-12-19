888ab38bf198cd8af58e2bf5220b602084d3e30b
dantzs <marcelohrs2005@gmail.com>
2023-12-19 14:11:33 -0300

    configurando o EntityFramework

---
+++ CrudCSharp/Data/ApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudCSharp.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) 
+        {
+        }
+
+
+    }
+}
---
+++ CrudCSharp/Model/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrudCSharp.Model
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="O campo não {0} é obrigatorio")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo não {0} é obrigatorio")]
+        [Range(1,int.MaxValue, ErrorMessage = "O preço deve ser maior que 0")]
+        public decimal Preco { get; set; }
+
+
+        [Required(ErrorMessage = "O campo não {0} é obrigatorio")]
+        public int QuantidadeEstoque { get; set; }
+
+        [Required(ErrorMessage = "O campo não {0} é obrigatorio")]
+        public string? Descricao { get; set; }
+
+    }
+}
